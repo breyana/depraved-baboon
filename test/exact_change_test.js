8931f047b45f46f800f4c9abcb7d5a192b5102e0
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-01-13 14:02:00 -0800

    Added in Exact Change test

---
+++ test/exact_change_test.js
@@ -0,0 +1,20 @@
+const {checkCashRegister} = require("../algorithms/exact_change.js");
+const {expect} = require('chai');
+
+describe('Exact Change', function() {
+  describe('Getting the change from the drawer', function() {
+    it('should return Insufficient Funds if cash-in-drawer is less than change due',
+    function() {
+      expect(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0],
+      ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0],
+      ["TWENTY", 0], ["ONE HUNDRED", 0]])).to.equal("Insufficient Funds");
+    });
+    it('should return Closed if cash-in-drawer is equal to the change due',
+    function() {
+      expect(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0],
+      ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0],
+      ["TWENTY", 0], ["ONE HUNDRED", 0]])).to.equal("Closed");
+    })
+
+  });
+});
